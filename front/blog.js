f81395c3297d3c7e668a98900e6e87f933e16d92
adeliamara <adeliamara13@gmail.com>
2023-06-02 22:38:45 -0300

    fix: correcao bug no like

--- front/blog.js
+++ front/blog.js
@@ -1,46 +1,68 @@
+
 const loadPosts = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/posts');
-      if (!response.ok) {
-        throw new Error('Erro ao carregar os posts');
-      }
-      const data = await response.json();
-      const postsArray = data.posts; // converter para um array no cliente
-
-
-      if (!Array.isArray(postsArray)) {
-        throw new Error('Os posts não são uma lista válida');
-      }
-  
-      postsArray.forEach(post => {
-        appendPost(post);
-      });
-    } catch (error) {
-      console.error(error);
-    }
+  const config = {
+    'method': 'GET',
+    'headers': {
+      'Content-Type': 'application/json'
+    },
   };
-  
+
+  const response = await fetch('http://localhost:3000/posts', config);
+  const postsArray = await response.json();
+  //const postsArray = data.posts; // converter para um array no cliente
+
+
+  if (!Array.isArray(postsArray)) {
+    throw new Error('Os posts não são uma lista válida');
+  }
+
+  postsArray.forEach(post => {
+    appendPost(post);
+  });
+
+};
+
 
 async function addPost() {
 
-    const newPost = {
-        "title": document.getElementById('post-title').value,
-        "text": document.getElementById('post-text').value,
-        "likes": 0
-    };
+  title = document.getElementById('post-title').value;
+  text =  document.getElementById('post-text').value;
 
-    const config = {
-        'method': 'POST',
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newPost)
-    };
+  const titleErrorElement = document.getElementById('title-error');
+  const textErrorElement = document.getElementById('text-error');
+  titleErrorElement.textContent = '';
+  textErrorElement.textContent = '';
 
-    const response = await fetch('http://localhost:3000/posts', config);
-    const post = await response.json();
 
-    appendPost(post);
+  if (!title) {
+    titleErrorElement.textContent = 'Please enter a post title.';
+    return;
+  }
+
+  if (!text) {
+    textErrorElement.textContent = 'Please enter the post text.';
+    return;
+  }
+  const newPost = {
+    "title": title,
+    "text": text,
+    "likes": 0
+  };
+
+
+  const config = {
+    'method': 'POST',
+    'headers': {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPost)
+  };
+
+  const response = await fetch('http://localhost:3000/posts', config);
+  const post = await response.json();
+
+  appendPost(post);
+  
 }
 
 async function addLike(id) {
@@ -52,43 +74,44 @@ async function addLike(id) {
   };
 
   const response = await fetch(`http://localhost:3000/posts/${id}/like`, config);
-  const post = await response.json();
+  const likes = await response.json();
 
   const likeCountElement = document.getElementById(id).querySelector('.like-count');
   if (likeCountElement) {
-    likeCountElement.innerText = post.likes;
+    likeCountElement.innerText = likes;
   }
 }
 
 
 
 
-async function appendPost (post){
-  const template = document.getElementById('post-template');
-    const postElement = document.importNode(template.content, true);
-    const likeButton = postElement.querySelector('button')
-    
-    const postTitle = postElement.querySelectorAll('h3')[0]
-    postTitle.innerText = post.title;
-    const postItens = postElement.querySelectorAll('p')
-    postItens[0].innerText = post.text;
-    const likeCountElement = postElement.querySelector('.like-count');
-    likeCountElement.innerText = post.likes
-    const article = postElement.querySelectorAll('article')[0]
-    article.id = post.id
 
-    document.getElementById('timeline').append(postElement);
-    likeButton.onclick = (event) => {
-      event.preventDefault(); // Impede o comportamento padrão do evento de clique
-      addLike(post.id);
-    };
-    
-  }
+
+function appendPost(post) {
+  const template = document.getElementById('post-template');
+  const postElement = document.importNode(template.content, true);
+  const likeButton = postElement.querySelector('button')
+
+  const postTitle = postElement.querySelectorAll('h3')[0]
+  postTitle.innerText = post.title;
+  const postItens = postElement.querySelectorAll('p')
+  postItens[0].innerText = post.text;
+  const likeCountElement = postElement.querySelector('.like-count');
+  likeCountElement.innerText = post.likes
+  const article = postElement.querySelectorAll('article')[0]
+  article.id = post.id
+
+  document.getElementById('timeline').append(postElement);
+  likeButton.onclick = (event) => {
+    event.preventDefault(); // Impede o comportamento padrão do evento de clique
+    addLike(post.id);
+  };
+}
 
 window.onload = () => {
-    const btnAddPost = document.getElementById('add-post')
-    btnAddPost.onclick = addPost;
+  const btnAddPost = document.getElementById('add-post')
+  btnAddPost.onclick = addPost;
 
-    loadPosts()
+  loadPosts()
 }
 
